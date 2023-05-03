0e621e22d5907c3b077fcf5fc3e6440463ed825a
JClark <jclarkgaming01@gmail.com>
2023-05-02 22:38:34 -0400

    method to register commands

---
+++ src/main/java/com/github/johnclark96/events/RegisterCommandEvent.java
@@ -0,0 +1,15 @@
+package com.github.johnclark96.events;
+
+import com.github.johnclark96.commands.OpenMenuCommand;
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.command.CommandSource;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class RegisterCommandEvent {
+//    @SubscribeEvent
+//    public static void onRegisterCommandEvent(RegisterCommandsEvent event) {
+////        CommandDispatcher<CommandSource> dispatcher = event.getDispatcher();
+////        OpenMenuCommand.register(dispatcher);
+//    }
+}
